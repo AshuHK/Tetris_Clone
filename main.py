ed7e6826b8bbfe06b782b61b927925ff5494eafc
Ashu Kher <ashukher1@gmail.com>
2020-05-18 10:28:09 -0400

    settings up and making the first shapes with 2D lists

--- main.py
+++ main.py
@@ -3,3 +3,27 @@ import random
 
 pygame.font.init() 
 
+# Global variables 
+window_width = 800 
+window_height = 700 
+
+play_width = 300 
+play_height = 600 
+
+block_size = 30
+
+top_left_x = (window_width - play_width) // 2 
+top_left_y = window_height - play_height
+
+# The shapes 
+
+S = [['.....',
+      '......',
+      '..00..',
+      '.00...',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
