a43ddd03fb48ded2211bac8a485dab61d0012ae8
Ashu Kher <ashukher1@gmail.com>
2020-05-18 12:44:07 -0400

    had to move the draw_grid code to draw_window

--- tetris.py
+++ tetris.py
@@ -160,22 +160,6 @@ def draw_text_middle(text, size, color, area):
 def draw_grid(area, row, column):
     """
     """
-    area.fill((0,0,0))
-
-    font = pygame.font.SysFont("comicsans", 60)
-    label = font.render("Tetris", 1, (255,255,255))
-
-    area.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
-
-    for i in range(len(grid)): 
-        for j in range(len(grid[0])): 
-            pygame.draw.rect(area, grid[i][j], (top_left_x + j * 30, top_left_y + i * 30, 30, 30), 0) 
-    
-    # draw the grid and border 
-    draw_grid(area, 20, 10) 
-    pygame.draw.rect(area, (255,0,0), (top_left_x, top_left_y, play_width, play_height), 5) 
-    
-    pygame.display.update()
 
 
 def clear_rows(grid, locked):
@@ -196,7 +180,23 @@ def draw_window(area):
     """
     """
 
-    pass
+    area.fill((0,0,0))
+
+    font = pygame.font.SysFont("comicsans", 60)
+    label = font.render("Tetris", 1, (255,255,255))
+
+    area.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
+
+    for i in range(len(grid)): 
+        for j in range(len(grid[0])): 
+            pygame.draw.rect(area, grid[i][j], (top_left_x + j * 30, top_left_y + i * 30, 30, 30), 0) 
+    
+    # draw the grid and border 
+    draw_grid(area, 20, 10) 
+    pygame.draw.rect(area, (255,0,0), (top_left_x, top_left_y, play_width, play_height), 5) 
+    
+    pygame.display.update()
+
 
 
 def main():
